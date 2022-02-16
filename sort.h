74370add0a21259dc9b67a6b6353ca277dfd4da9
maycolroa <lehallo@hotmail.com>
2022-02-15 23:49:39 -0500

    0x1B. C - Sorting algorithms & Big O  0.07 15/02/22

--- sort.h
+++ sort.h
@@ -17,12 +17,13 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
+void lomuto_alg(int *point_array, int start, int end, size_t size);
+int partition(int *array, size_t start, size_t end, size_t size);
 
 #endif
